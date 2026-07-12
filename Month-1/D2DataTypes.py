be9a99af6d3a264886093fa7ada4011a2ed16dbe
dhandedevsh8402 <dhandedevsh8402@gmail.com>
2026-07-12 15:35:53 +0530

    Practiced about Datatypes

---
+++ Month-1/D2DataTypes.py
@@ -0,0 +1,47 @@
+age = 20
+height = 5.4
+complex_number = 3+4j
+
+print(f"Value: {age} \n Type: {type(age).__name__}")
+print()
+
+print(f"Value: {height} \n Type: {type(height).__name__}")
+print()
+
+print(f"Value: {complex_number} \n Type: {type(complex_number).__name__}\n")
+print("-"*50)
+
+
+#--------------------------------------------------
+
+a = False
+print(f"Datatype of {a} is {type(a).__name__}")
+print()
+
+complex_num = 3j
+print(f"Datatype of {complex_num} is {type(complex_num).__name__}")
+print()
+
+x= -100
+print(f"Datatype of {x} is {type(x).__name__}")
+print()
+
+z=2.718
+print(f"Datatype of {z} is {type(z).__name__}")
+print()
+print("-"*50)
+
+#-----------------------------------------------------------------------
+
+x = 10
+print(f"Datatype of {x} :{type(x).__name__}")
+print()
+
+x = 5.5
+print(f"Datatype of {x} :{type(x).__name__}")
+print()
+
+x = 2+7j
+print(f"Datatype of {x} :{type(x).__name__}")
+print()
+print("-"*50)
